8fa12c3ef95eab1ef63d7e879d912f2f997af597
Juan Cardona <inbox@branamail.xyz>
2025-08-24 16:32:00 -0500

    Add deploy.yml and add base param in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/felines_website',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
